63d8f87e3fe726af27485d1d89c2dadf2f57f583
marcoweb <marcocarvalho.web@gmail.com>
2024-03-22 12:18:33 +0000

    Calculadora

--- app/src/main/java/application/controller/CalcController.java
+++ app/src/main/java/application/controller/CalcController.java
@@ -3,6 +3,7 @@ package application.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -17,6 +18,30 @@ public class CalcController {
             @RequestParam("numA") int nA,
             @RequestParam("numB") int nB) {
         ui.addAttribute("resultado", nA + nB);
+        return "calc/form";
+    }
+
+    @RequestMapping(value = "/calc", method = RequestMethod.POST)
+    public String calc(Model ui,
+    @RequestParam("numA") int nA,
+    @RequestParam("numB") int nB,
+    @RequestParam("operacao") String op) {
+        double resultado = 0;
+        switch (op) {
+            case "soma":
+                resultado = nA + nB;
+                break;
+            case "subtracao":
+                resultado = nA - nB;
+                break;
+            case "multiplicacao":
+                resultado = nA * nB;
+                break;
+            case "divisao":
+                resultado = nA / nB;
+                break;
+        }
+        ui.addAttribute("resultado", resultado);
         return "calc/form";
     }
 }
--- app/src/main/webapp/WEB-INF/calc/form.jsp
+++ app/src/main/webapp/WEB-INF/calc/form.jsp
@@ -6,11 +6,18 @@
         <title>Calc</title>
     </head>
     <body>
-        <form action="/soma" method="post">
+        <form action="/calc" method="post">
             <label>Num A</label>
             <input type="number" name="numA" />
             <label>Num B</label>
             <input type="number" name="numB" />
+            <label>Operação</label>
+            <select name="operacao">
+                <option value="soma">Soma</option>
+                <option value="subtracao">Subtração</option>
+                <option value="multiplicacao">Multiplicação</option>
+                <option value="divisao">Divisão</option>
+            </select>
             <button type="submit">Calcular</button>
         </form>
         <hr />
